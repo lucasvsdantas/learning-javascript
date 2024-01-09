d0934fdd3168e54df58ef0f2d98cc7db35c3c056
Lucas Vinicius Sales Dantas <lucasviniciussd2@gmail.com>
2024-01-09 16:54:14 -0300

    while and do/while

---
+++ aula13/ambiente.js
@@ -0,0 +1,20 @@
+var i = 1;
+do{
+    console.log(`Passo ${i}`);
+    i++;
+}while(i <= 6)
+/*
+var i = 1;
+while(i <= 500){
+    console.log(`Passo ${i}`);
+    i++;
+}
+*/
+/*
+console.log(`Tudo bem?`);
+console.log(`Tudo bem?`);
+console.log(`Tudo bem?`);
+console.log(`Tudo bem?`);
+console.log(`Tudo bem?`);
+console.log(`Tudo bem?`);
+*/
