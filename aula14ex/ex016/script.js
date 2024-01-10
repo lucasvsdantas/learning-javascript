c22ce545bdb3454694f672b63dfcd33cc02731f2
Lucas Vinicius Sales Dantas <lucasviniciussd2@gmail.com>
2024-01-10 16:23:19 -0300

    Adjustments to "Let's count"

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -14,11 +14,21 @@ function contar(){
     var passo = Number(tpasso.value);
 
     cont.innerHTML = "<p></p>";
-    if(ini == undefined || fim == undefined || passo == undefined){
+    if(passo == 0){
+        alert("Passo 0 não pode ser utilizado, considerando Passo = 1");
+        passo = 1;
+    }
+    if(ini-fim == 0){
         cont.innerText = `Valor indefinido`;
-    }else if(passo == 0){
-        cont.innerText = `Valor passo não pode ser 0`;
-    }else{
+    }
+    else if(ini > fim){
+        for(ini; ini>=fim; ini-=passo){
+            cont.innerHTML += `${ini} &#x1F449; `;
+            main.style.height = `${200+cont.offsetHeight}px`
+        }
+        cont.innerHTML += `&#x1F3C1;`;
+    }
+    else{
         for(ini; ini<=fim; ini+=passo){
             cont.innerHTML += `${ini} &#x1F449; `;
             main.style.height = `${200+cont.offsetHeight}px`
