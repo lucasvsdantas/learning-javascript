cadc89be7363f2475990c7733e6d632cd06ee428
Lucas Vinicius Sales Dantas <lucasviniciussd2@gmail.com>
2024-01-10 17:35:09 -0300

    Ex "Multiplication table"

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,23 +1,11 @@
-var agora = new Date();
-var hora = agora.getHours();
-var horario = document.getElementById("texto");
-var img = document.getElementById("imagem");
-hora = 20;
-horario.innerHTML = `Agora são ${hora} horas`;
+var botao = document.getElementById("botao");
+var tabuada = document.getElementById("tabuada");
 
-if(hora >= 6 && hora < 12){
-    img.style.backgroundImage = "url('imagem/manhã.jpg')";
-    document.body.style.backgroundColor = "#21fff4";
-}else if(hora >= 6 && hora < 16){
-    img.style.backgroundImage = "url('imagem/tarde.jpg')";
-    document.body.style.backgroundColor = "#b0fffb";
-}else if(hora >= 6 && hora < 18){
-    img.style.backgroundImage = "url('imagem/entardecer.jpg')";
-    document.body.style.backgroundColor = "#527ffa";
-}else{
-    img.style.backgroundImage = "url('imagem/noite.jpg')";
-    document.body.style.backgroundColor = "#0003a1";
-    document.getElementById("header").style.color = "white";
-    document.getElementById("footer").style.color = "white";
+function calcular(){
+    var tnum = document.getElementById("inum");
+    var num = Number(tnum.value);
+    tabuada.innerHTML = ""
+    for(var i = 1; i<=10; i++){
+        tabuada.innerHTML += `<p>${num} X ${i} = ${num*i}</p>`
+    }
 }
-
