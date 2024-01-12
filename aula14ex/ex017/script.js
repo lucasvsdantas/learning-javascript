9cdc731ecd8714176a8d9d0721077405b0764b74
Lucas Vinicius Sales Dantas <lucasviniciussd2@gmail.com>
2024-01-12 15:43:27 -0300

    Adjustments to "Multiplication Table"

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,11 +1,14 @@
-var botao = document.getElementById("botao");
-var tabuada = document.getElementById("tabuada");
-
 function calcular(){
+    var tabuada = document.getElementById("itabuada");
     var tnum = document.getElementById("inum");
     var num = Number(tnum.value);
-    tabuada.innerHTML = ""
+    
+    tabuada.innerHTML = "";
+
     for(var i = 1; i<=10; i++){
-        tabuada.innerHTML += `<p>${num} X ${i} = ${num*i}</p>`
+        let item = document.createElement('option');
+        item.text = `${num} X ${i} = ${num*i}`;
+        item.value = `tab${i}`
+        tabuada.appendChild(item);
     }
 }
